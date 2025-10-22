a5274861e6657afd6c2ee916e74f9d62f293fd2f
Paul Neenan <paulneenan@Mac.lan>
2025-10-22 15:23:23 -0400

    moves calendar

---
+++ src/app/calendar/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import CalendarSection from "../components/events/CalendarSection";
+
+export default function CalendarPage() {
+  return (
+    <main className="container mx-auto px-4 py-12 mt-10 max-w-5xl">
+
+      <CalendarSection />
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main>
       <HeroCarousel />
-      <CalendarSection />
+      {/* <CalendarSection /> */}
       {/* <AboutSection /> */}
       {/* Remove the SEO info section for production if desired */}
       {/* <SEOInfoSection /> */}
